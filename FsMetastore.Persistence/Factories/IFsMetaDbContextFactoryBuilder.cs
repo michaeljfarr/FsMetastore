49e89bf3ad70e2cfd91e60f66f7788cb70d67b44
Michael Farr <michaeljfarr@hotmail.com>
2021-03-08 09:58:52 +1100

    Add IFsMetaDbContextFactoryBuilder.ReadSource

--- FsMetastore.Persistence/Factories/IFsMetaDbContextFactoryBuilder.cs
+++ FsMetastore.Persistence/Factories/IFsMetaDbContextFactoryBuilder.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Threading.Tasks;
+using FsMetastore.Model.Batch;
 
 namespace FsMetastore.Persistence.Factories
 {
     public interface IFsMetaDbContextFactoryBuilder
     {
         IFsMetaDbContextFactory CreateForDrive(Guid driveId);
+        Task<BatchSource> ReadSource(string fsMetaDbPath);
     }
 }
--- FsMetastore.Persistence/Factories/SqliteFsMetaDbContextFactoryBuilder.cs
+++ FsMetastore.Persistence/Factories/SqliteFsMetaDbContextFactoryBuilder.cs
@@ -1,5 +1,10 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
+using FsMetastore.Model.Batch;
+using FsMetastore.Persistence.IO.FileBatches;
+using FsMetastore.Persistence.IO.FsMetaDb;
+using FsMetastore.Persistence.IO.Test;
 using Microsoft.Extensions.Options;
 
 namespace FsMetastore.Persistence.Factories
@@ -22,6 +27,21 @@ namespace FsMetastore.Persistence.Factories
         {
             var baseFolder = Path.Combine(_config.CurrentValue.ConnectionString, driveId.ToString("N"));
             return new SqliteFsMetaDbContextFactory(baseFolder);
+        }
+        
+        public async Task<BatchSource> ReadSource(string fsMetaDbPath)
+        {
+            var batchIoConfig = new BatchIOConfig
+            {
+                BatchPathRoot = fsMetaDbPath,
+                BatchSourceEncoding = BatchSourceEncoding.Utf8,
+                SourcePath = null
+            };
+            var scanDbBatchIOFactory =
+                new IO.FsScanStream.FsScanStreamBatchIOFactory(batchIoConfig,
+                    new NullTestOutputer());
+            var metaBatchReader = new MetaBatchReader(scanDbBatchIOFactory);
+            return await metaBatchReader.ReadSourceAsync();
         }
     }
 }
